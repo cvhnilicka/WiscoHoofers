b6c638d973405cc27ef579f8fbb493ea0e531326
sheila hnilicka <jshnilicka@wi.rr.com>
2015-11-07 19:20:18 -0600

    working version

--- Wisconsin Hoofers/SailingViewController.swift
+++ Wisconsin Hoofers/SailingViewController.swift
@@ -9,18 +9,27 @@
 import UIKit
 import GoogleMaps
 
-class SailingViewController: UIViewController {
+class SailingViewController: UIViewController, CLLocationManagerDelegate  {
     
     
     @IBOutlet weak var mapView: GMSMapView!
-   
-    let locationManager = CLLocationManager()
+    
+
+    var locationManager = CLLocationManager()
+    
+    var didFindMyLocation = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
         
-        var camera = GMSCameraPosition.cameraWithLatitude(45, longitude: 45, zoom: 15)
+        mapView.addObserver(self, forKeyPath: "myLocation", options: NSKeyValueObservingOptions.New, context: nil);
+
         
-        mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera);
+        var camera = GMSCameraPosition.cameraWithLatitude(0, longitude: 45, zoom: 15)
+        
+        mapView = GMSMapView.mapWithFrame(CGRectMake(0, 0, 320, 320), camera: camera);
         
         self.navigationController?.navigationBarHidden = false;
         UIGraphicsBeginImageContext(self.view.frame.size)
@@ -39,6 +48,22 @@ class SailingViewController: UIViewController {
       
        
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func observeValueForKeyPath(keyPath: String?, ofObject object: AnyObject?, change: [String : AnyObject]?, context: UnsafeMutablePointer<Void>) {
+        if !didFindMyLocation {
+            let myLocation: CLLocation = change![NSKeyValueChangeNewKey] as! CLLocation
+            mapView.camera = GMSCameraPosition.cameraWithTarget(myLocation.coordinate, zoom: 10.0)
+            mapView.settings.myLocationButton = true
+            
+            didFindMyLocation = true
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        if status == CLAuthorizationStatus.AuthorizedWhenInUse {
+            mapView.myLocationEnabled = true
+        }
     }
 
     
