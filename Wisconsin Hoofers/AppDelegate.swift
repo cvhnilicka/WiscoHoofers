2ef3d67197aeb101bf94680c68d30140fc88f78f
Gaoyuan Chen <289312802@qq.com>
2015-11-13 18:11:27 -0600

    Add Facebook SDK and login button in SignOnViewController

--- Wisconsin Hoofers/AppDelegate.swift
+++ Wisconsin Hoofers/AppDelegate.swift
@@ -11,7 +11,7 @@ import Google
 import GoogleMaps
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
@@ -23,56 +23,62 @@ class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
         var configureError: NSError?
         GGLContext.sharedInstance().configureWithError(&configureError)
         assert(configureError == nil, "Error configuring Google services: \(configureError)")
-        
-        GIDSignIn.sharedInstance().delegate = self
-        
-        
-        
+
         // Override point for customization after application launch.
-        return true
+        return FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
+        
     }
     func application(application: UIApplication,
         openURL url: NSURL, sourceApplication: String?, annotation: AnyObject) -> Bool {
-            return GIDSignIn.sharedInstance().handleURL(url,
+            
+            return FBSDKApplicationDelegate.sharedInstance().application(
+                application,
+                openURL: url,
                 sourceApplication: sourceApplication,
                 annotation: annotation)
+            
+//            return GIDSignIn.sharedInstance().handleURL(url,
+//                sourceApplication: sourceApplication,
+//                annotation: annotation)
+//            
+           
     }
     
     // [START signin_handler]
-    func signIn(signIn: GIDSignIn!, didSignInForUser user: GIDGoogleUser!,
-        withError error: NSError!) {
-            if (error == nil) {
-                // Perform any operations on signed in user here.
-                let userId = user.userID                  // For client-side use only!
-                let idToken = user.authentication.idToken // Safe to send to the server
-                let name = user.profile.name
-                let email = user.profile.email
-                // [START_EXCLUDE]
-                NSNotificationCenter.defaultCenter().postNotificationName(
-                    "ToggleAuthUINotification",
-                    object: nil,
-                    userInfo: ["statusText": "Signed in user:\n\(name)"])
-                // [END_EXCLUDE]
-            } else {
-                print("\(error.localizedDescription)")
-                // [START_EXCLUDE silent]
-                NSNotificationCenter.defaultCenter().postNotificationName(
-                    "ToggleAuthUINotification", object: nil, userInfo: nil)
-                // [END_EXCLUDE]
-            }
-    }
+//    func signIn(signIn: GIDSignIn!, didSignInForUser user: GIDGoogleUser!,
+//        withError error: NSError!) {
+//            if (error == nil) {
+//                // Perform any operations on signed in user here.
+//                let userId = user.userID                  // For client-side use only!
+//                let idToken = user.authentication.idToken // Safe to send to the server
+//                let name = user.profile.name
+//                let email = user.profile.email
+//                // [START_EXCLUDE]
+//                NSNotificationCenter.defaultCenter().postNotificationName(
+//                    "ToggleAuthUINotification",
+//                    object: nil,
+//                    userInfo: ["statusText": "Signed in user:\n\(name)"])
+//                // [END_EXCLUDE]
+//            } else {
+//                print("\(error.localizedDescription)")
+//                // [START_EXCLUDE silent]
+//                NSNotificationCenter.defaultCenter().postNotificationName(
+//                    "ToggleAuthUINotification", object: nil, userInfo: nil)
+//                // [END_EXCLUDE]
+//            }
+//    }
 
-    func signIn(signIn: GIDSignIn!, didDisconnectWithUser user:GIDGoogleUser!,
-        withError error: NSError!) {
-            // Perform any operations when the user disconnects from app here.
-            // [START_EXCLUDE]
-            NSNotificationCenter.defaultCenter().postNotificationName(
-                "ToggleAuthUINotification",
-                object: nil,
-                userInfo: ["statusText": "User has disconnected."])
-            // [END_EXCLUDE]
-    }
-
+//    func signIn(signIn: GIDSignIn!, didDisconnectWithUser user:GIDGoogleUser!,
+//        withError error: NSError!) {
+//            // Perform any operations when the user disconnects from app here.
+//            // [START_EXCLUDE]
+//            NSNotificationCenter.defaultCenter().postNotificationName(
+//                "ToggleAuthUINotification",
+//                object: nil,
+//                userInfo: ["statusText": "User has disconnected."])
+//            // [END_EXCLUDE]
+//    }
+//
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
@@ -89,6 +95,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
 
     func applicationDidBecomeActive(application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+         FBSDKAppEvents.activateApp()
     }
 
     func applicationWillTerminate(application: UIApplication) {
---
+++ Wisconsin Hoofers/Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Bridging-Header.h
+//  Wisconsin Hoofers
+//
+//  Created by Gaoyuan Chen on 11/13/15.
+//  Copyright © 2015 Cormick Hnilicka. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
+#endif /* Bridging_Header_h */
